c605b41b2d2b295d772a9dcc43b2250f281b9878
Yara Nolf <yara.nolf@telenet.be>
2019-03-27 10:15:15 +0100

    basic

--- lab4/app.js
+++ lab4/app.js
@@ -1,6 +1,3 @@
-
-console.log("🙈 🙉 🙊");
-
 class Weather{
     constructor(API_KEY){
         this.API_KEY = API_KEY;
@@ -17,27 +14,29 @@ class Weather{
         console.log("Getting location");
         navigator.geolocation.getCurrentPosition(position => { //function(position)
         console.log("found you");
-       let lat=position.coords.latitude;
+        let lat = position.coords.latitude;
         let lng = position.coords.longitude;
-        this.getWeather(lat,lng);
-        console.log(position);
-
+        this.getWeather(lat, lng);
     }, err => {
         console.log("mispoes");
     });
     }
+
+    getWeather(lat, lng){
+        let url = `https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/${this.API_KEY}/${lat},${lng}?units=si`;
+        fetch(url)
+        .then(response => {
+            return response.json();
+        })
+        .then(json => {
+            let temp = document.createElement("h1");
+            temp.innerHTML = json.currently.summary;
+            document.querySelector("body").appendChild(temp);
+            //console.log(json.currently.summary);
+            //console.log(json);
+        });
+    }
 }
 
-getWeather(lat, lng);{
-    //ajax call / XHR
-    //https://api.darksky.net/forecast/70cbb5b3805551203ecfbda0653231af/37.8267,-122.4233
+let app = new Weather('87d0b90aaed973175e9e8896d40716eb');
 
-    let url = `https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/${this.API_KEY}/${lat},${lng}?units=si`;
-    fetch(url)
-    .then(response =>{
-        return response.json();
-    })
-    .then(json=>{
-        console.log(json);
-});
-}
